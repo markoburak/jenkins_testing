b5fc5af7bede73f8d22186d12b72718b193bd99b
markoburakINX <marko.burak@inx.co>
2022-04-07 21:41:47 -0400

    jenkinks update

--- jenkins.py
+++ jenkins.py
@@ -48,20 +48,35 @@ class TestOneNeed:
     @allure.severity(severity_level="CRITICAL")
     def test_button(self):
         try:
-            print(general_selectors['main_screen']['button'])
             # get the title by the selector
-            button = WebDriverWait(self.driver, 10).until(
+            title = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located(
-                    (By.CSS_SELECTOR, "#__docusaurus > div.main-wrapper > header > div > div > a")
+                    (By.CSS_SELECTOR, general_selectors['main_screen']['title'])
                 )
             )
-            print()
-            print(button.text)
             # check if text of the title element is correct
-            print()
-
             try:
-                assert button.text == "Get Started2"
+                assert title.text == "Electron React Boilerplate"
+            finally:
+                if AssertionError:
+                    allure.attach(self.driver.get_screenshot_as_png(), name="Invalid_title",
+                                  attachment_type=allure.attachment_type.PNG)
+        except TimeoutException:
+            print("bad")
+
+    @allure.description("Validate Button2")
+    @allure.severity(severity_level="CRITICAL")
+    def test_button2(self):
+        try:
+            # get the title by the selector
+            title = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, general_selectors['main_screen']['title'])
+                )
+            )
+            # check if text of the title element is correct
+            try:
+                assert title.text == "Electron React Boilerplate"
             finally:
                 if AssertionError:
                     allure.attach(self.driver.get_screenshot_as_png(), name="Invalid_title",
